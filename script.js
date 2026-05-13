0759944fb180e78d654588beadd46f61063ff548
victorgabriel0026 <victorgabrielcardosogalvao2006@gmail.com>
2026-05-12 21:15:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+
+let m1 = 6
+let m2 = 6
+let m3 = 21-(m2+m1)
+let m4 = 3/(m3+m2+m1)
+//let m5 = 0
+let m6 = (m4-(5*2))
+console.log ("voce precisa tirar ",m3," para passar na prova da n3")
+if (m4 >= 7){
+    console.log("Voce foi aprovado")
+}else {
+    console.log("voce nao passou, precisa ir para exame")
+    console.log("Voce precisa tirar "(m4-5*2))
+}
+// if (m3 < 5){
+//     console.log("parabem voce passou no exame")
+// }else {
+//     console.log("Infelizmente voce reprovou")
+// }
+
+
+
+
+
+
+
+
+// let n1 = 9
+// let n2 = 5
+// let n3 = 8
+// let mn = ((n1+n2+n3)/3)
+// console.log(mn)
+
+// if (mn >= 7){
+//     console.log("Parabens voce pasoou de ano")
+// } else {
+//     console.log("Infelizmente nao passou")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = 5;
+// let b = 20;
+// let idade = 19
+// let casado = false
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b) // divisao
+// console.log(a=b) // recebe
+// console.log(a==b)// igual
+// console.log(a!=b) //diferente
+// if (idade >= 18) {
+//     console.log("voce é maior de idade")
+// } else () {
+//     console.log("Voce é nemor de idade")
+// }
+
+// if (casado) {
+//     ("ta lascado")
+// } else () {
+//     ("aproveitou a vida")
+// }
+
+
+// let nome = "victor";
+// let sobrenome = "gabriel"
+// let idade = 19;
+// let lascado = false;
+
+// console.log("ola mundo, bem vindo ", nome, sobrenome );
+// console.log(idade);
+// console.log(lascado);
+
+// alert("bem vindo ao meu site");
